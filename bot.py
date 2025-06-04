ab130f08ef2f69db61e2a026b584e0badf358277
AttilaSz <szszattila2@gmail.com>
2025-06-04 18:05:20 +0300

    study start session implemented

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
 from discord.ext import commands
 import discord
+from dataclasses import dataclass
 
 with open('token.txt', 'r') as f:
     BOT_TOKEN = f.read().strip()
 
 CHANNEL_ID = 308289885767204864
 
+@dataclass
+class Session:
+    is_active: bool = False
+    start_time: int = 0
+
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+session = Session()
 
 @bot.event
 async def on_ready():
@@ -15,8 +22,15 @@ async def on_ready():
     await channel.send("Hello! Study bot is ready!")
 
 @bot.command()
-async def add(ctx, x, y):
-    result = int(x) + int(y)
-    await ctx.send(f"{x} + {y} = {result}")
+async def start(ctx):
+    if session.is_active:
+        await ctx.send("A session is already active!")
+        return
+
+    session.is_active = True
+    session.start_time = ctx.message.created_at.timestamp()
+    human_readable_time = ctx.message.created_at.strftime("%H:%M:%S")
+    await ctx.send(f"New session started at {human_readable_time}")
+
 
 bot.run(BOT_TOKEN)
